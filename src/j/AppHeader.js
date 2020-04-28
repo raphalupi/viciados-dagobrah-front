0ce7e17019267bc521c8d9f2b5721577fd4a29ac
Raphael Lupchinski <raphael.lupchinski@quizlet.com>
2020-04-28 02:21:43 -0300

    Replacing favicon and logos for PWA for the new logo. Adding the logo to the app header

--- src/j/AppHeader.js
+++ src/j/AppHeader.js
@@ -5,15 +5,22 @@ import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 import { Menu as MenuIcon } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 
+import logoImage from '../i/logo192.png';
+
 const useStyles = makeStyles(theme => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(1),
     [theme.breakpoints.up('md')]: {
       display: 'none'
     }
+  },
+  logoImg: {
+    height: 32,
+    marginRight: theme.spacing(1),
+    width: 32
   }
 }));
 
@@ -22,18 +29,19 @@ function AppHeader(props) {
   const classes = useStyles();
 
   return (
-    <AppBar position="fixed" className={classes.appBar}>
+    <AppBar position='fixed' className={classes.appBar}>
       <Toolbar>
         <IconButton
-          color="inherit"
-          aria-label="Open drawer"
-          edge="start"
+          color='inherit'
+          aria-label='Open drawer'
+          edge='start'
           onClick={onOpenClick}
           className={classes.menuButton}
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap>
+        <img className={classes.logoImg} src={logoImage} />
+        <Typography variant='h6' noWrap>
           GA-nalysis
         </Typography>
       </Toolbar>
